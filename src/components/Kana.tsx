46c86a5da905cb8114e928102d88a37e4011de91
Sammy Samkough <sammysamkough@gmail.com>
2020-05-11 23:31:14 -0400

    Added Kana page

---
+++ src/components/Kana.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Kana = ({ character }: { character: string }) => {
+    return (
+        <div>
+            <p>japanese character</p>
+        </div>
+    );
+};
+
+export default Kana;
