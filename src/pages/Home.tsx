d690b6b6b5f1b6e0a21f4862989dba9796fd509b
Sammy Samkough <sammysamkough@gmail.com>
2020-05-11 23:31:27 -0400

    Added content to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Row, Col, Checkbox, Button } from 'antd';
+import { Link } from 'react-router-dom';
+
+const checkboxOptions: string[] = [
+    'Hiragana',
+    'Katakana',
+    'Hiragana Dakuten',
+    'Katakana Dakuten',
+    'Hiragana Combo',
+    'Katakana Combo',
+];
 
 const Home = () => {
+    const [checkedList, setCheckedList] = useState([] as any);
+    const [indeterminate, setIndeterminate] = useState(false);
+    const [checkAll, setCheckAll] = useState(false);
+
+    const onChange = (checkedList: any) => {
+        setCheckedList(checkedList);
+        setIndeterminate(!!checkedList.length && checkedList.length < checkboxOptions.length);
+        setCheckAll(checkedList.length === checkboxOptions.length);
+    };
+
+    const onCheckAllChange = (e: any) => {
+        setCheckedList(e.target.checked ? checkboxOptions : []);
+        setIndeterminate(false);
+        setCheckAll(e.target.checked);
+    };
+
+    const onSubmit = () => {};
+
     return (
         <div>
-            <h1>Home</h1>
+            <Row>
+                <Col>
+                    <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
+                        Check all
+                    </Checkbox>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Checkbox.Group
+                        options={checkboxOptions}
+                        value={checkedList}
+                        onChange={onChange}
+                        className="checkbox-group"
+                    ></Checkbox.Group>
+                    <br />
+                    <br />
+                    <Link to={{ pathname: '/playground', state: checkedList }}>
+                        <Button onClick={onSubmit}>Start</Button>
+                    </Link>
+                </Col>
+            </Row>
         </div>
     );
 };
