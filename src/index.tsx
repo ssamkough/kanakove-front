3e000638e4a2509d41459732047437719467b953
Sammy Samkough <sammysamkough@gmail.com>
2021-01-11 02:34:59 -0500

    updated google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 
-ReactGA.initialize('G-ECNW9FPPSL');
-ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById('root'));
